36453e9568789038830eb171477605738b3670ea
Wauzmons <dev@wauz.eu>
2019-07-01 20:21:33 +0200

    Cleanup

--- README.md
+++
@@ -1,2 +0,0 @@
-# WauzCore
-:blue_book: Documentation: https://wauz.atlassian.net/wiki/spaces/W/overview
--- src/main/java/eu/wauz/wauzcore/menu/util/WauzInventory.java
+++
@@ -1,9 +0,0 @@
-package eu.wauz.wauzcore.menu.util;
-
-import org.bukkit.event.inventory.InventoryClickEvent;
-
-public interface WauzInventory {
-	
-	public void selectMenuPoint(InventoryClickEvent event);
-	
-}
