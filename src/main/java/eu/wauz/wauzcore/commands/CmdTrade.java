fb1f858bf5fdf533831d27588ce33cad080fa8cd
eddshine <65357604+eddshine@users.noreply.github.com>
2020-05-23 08:07:24 +0800

    Create CmdTrade.java

---
+++ src/main/java/eu/wauz/wauzcore/commands/CmdTrade.java
@@ -0,0 +1,21 @@
+package eu.wauz.wauzcore.commands;
+
+import org.bukkit.command.CommandSender;
+
+import eu.wauz.wauzcore.commands.execution.WauzCommand;
+
+public class CmdTrade implements WauzCommand {
+
+	@Override
+	public String getCommandId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean executeCommand(CommandSender sender, String[] args) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
