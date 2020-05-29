17613196762a0be3518d435351976a56ad811520
eddshine <65357604+eddshine@users.noreply.github.com>
2020-05-30 00:49:51 +0800

    Trade (Incomplete)

--- src/main/java/eu/wauz/wauzcore/menu/TradeMenu.java
+++ src/main/java/eu/wauz/wauzcore/menu/TradeMenu.java
@@ -1,9 +1,16 @@
 package eu.wauz.wauzcore.menu;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
 
 public class TradeMenu {
 	public static void openTrade(Player requestingPlayer, Player requestedPlayer) {
+		String menuTitle = ChatColor.BLACK + "" + ChatColor.BOLD + requestingPlayer.getName() + ChatColor.DARK_GREEN + "    " + ChatColor.BOLD + "On Trade" + "    " + ChatColor.BLACK + "" + ChatColor.BOLD + requestedPlayer.getName() ;
+		Inventory tradeMenu = Bukkit.createInventory(null, 27, menuTitle );
 		
+		requestingPlayer.openInventory(tradeMenu);
+		requestedPlayer.openInventory(tradeMenu);
 	}
 }
