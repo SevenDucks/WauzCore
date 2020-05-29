e615acccd01e6772df56c9d8a66f5b85e2d534c2
eddshine <65357604+eddshine@users.noreply.github.com>
2020-05-30 00:50:29 +0800

    Revert "Trade (Incomplete)"
    This reverts commit 17613196762a0be3518d435351976a56ad811520.

--- src/main/java/eu/wauz/wauzcore/players/WauzPlayerTrade.java
+++ src/main/java/eu/wauz/wauzcore/players/WauzPlayerTrade.java
@@ -12,15 +12,20 @@ import org.bukkit.entity.Player;
 import eu.wauz.wauzcore.WauzCore;
 import eu.wauz.wauzcore.system.nms.WauzNmsClient;
 
+@SuppressWarnings("unused")
 public class WauzPlayerTrade {
 	/**
 	 * Players currently trading with other player
 	 */
-	private static ArrayList<String> playersOnTrading = new ArrayList<String>();
+	private static ArrayList<String> PlayerCurrentlyTrading = new ArrayList<String>();
 	/**
 	 * Max Player range for each other to trade.
 	 */
 	public static final int MAX_PLAYER_BLOCK_RANGE_FOR_TRADE = 5;
+	/**
+	 * A map of send trade requests as player uuids.
+	 */
+	private static Map<String,String> TradeRequestMap = new HashMap<>();
 	/**
 	 * Checks if a player can add another player as friend.
 	 * 
@@ -31,6 +36,7 @@ public class WauzPlayerTrade {
 	 * 
 	 * 
 	 */
+    @SuppressWarnings("unlikely-arg-type")
 	public static boolean onTradeCheck(Player requestingPlayer, OfflinePlayer requestedPlayerName) {
     	if(requestingPlayer.getUniqueId().equals(requestedPlayerName.getUniqueId())) {
     		requestingPlayer.sendMessage(ChatColor.RED + "You cannot send trade request to yourself.");
@@ -41,57 +47,24 @@ public class WauzPlayerTrade {
     	if(playersDistance > 7) {
     		requestingPlayer.sendMessage(ChatColor.RED + "The requested player is too far.");
     	}
-    	if(playersOnTrading.contains(requestedPlayer.getName())) {
+    	if(PlayerCurrentlyTrading.contains(requestedPlayer.getUniqueId())) {
     		requestingPlayer.sendMessage(ChatColor.RED + "The player you requested for trade is currently trading with someone. Please, Try again later.");
     		return false;
     	}
 
 		return true;
     }
-    /**
-     * Accept a trade from requesting player
-     * @param requestingPlayer
-     * @param requestedPlayer
-     * @return
-     */
-    public static boolean acceptTrade(Player requestingPlayer, Player requestedPlayer) {
-    	playersOnTrading.add(requestingPlayer.getName());
-    	playersOnTrading.add(requestedPlayer.getName());
-		WauzNmsClient.nmsChatCommand(requestedPlayer.getPlayer(), "trade " + requestingPlayer.getName(),
-				ChatColor.YELLOW + requestingPlayer.getName() + " wants to trade! " +
-				"To accept:", false);
-		requestingPlayer.sendMessage(ChatColor.YELLOW + "A trade request was sent to " + requestedPlayer.getName() + "!");
-		return true;
-    }
-    /**
-     * decline a trade from requestingPlayer
-     * @param requestingPlayer
-     * @param requestedPlayer
-     * @return
-     */
-    public static boolean denyTrade(Player requestingPlayer, Player requestedPlayer) {
-    	playersOnTrading.add(requestingPlayer.getName());
-    	playersOnTrading.add(requestedPlayer.getName());
-		WauzNmsClient.nmsChatCommand(requestedPlayer.getPlayer(), "trade " + requestingPlayer.getName(),
-				ChatColor.YELLOW + requestingPlayer.getName() + " wants to trade! " +
-				"To accept:", false);
-		requestingPlayer.sendMessage(ChatColor.YELLOW + "A trade request was sent to " + requestedPlayer.getName() + "!");
-		return true;
-    }
     
-    public static boolean Trading(Player requestingPlayer, Player requestedPlayer) {
-
+    public static boolean Trading(Player requestingPlayer, String requestedPlayerName) {
+		OfflinePlayer requestedPlayer = WauzCore.getOfflinePlayer(requestedPlayerName);
+		if(requestedPlayer == null) {
+			requestingPlayer.sendMessage(ChatColor.RED + "The requested player is unknown!");
+			return false;
+		}
 		if(!onTradeCheck(requestingPlayer, requestedPlayer)) {
 			return false;
 		}
-		Player requestedOnlinePlayer = requestedPlayer.getPlayer();
-		if(requestedOnlinePlayer == null) {
-			requestingPlayer.sendMessage(ChatColor.RED + "The requested player is not online!");
-			return false;
-		}else {
-			acceptTrade(requestingPlayer, requestedPlayer);
-			denyTrade(requestingPlayer, requestedPlayer);
-		}
+		
 		return true;
     	
     }
