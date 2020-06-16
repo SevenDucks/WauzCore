17edef1bca55ef40e174a7ac72de51383f7ae271
Wauzmons <dev@wauz.eu>
2020-06-16 20:57:46 +0200

    WauzCore 0.6.03 Currency Configuration

---
+++ src/main/java/eu/wauz/wauzcore/data/CurrencyConfigurator.java
@@ -0,0 +1,64 @@
+package eu.wauz.wauzcore.data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import eu.wauz.wauzcore.data.api.GlobalConfigurationUtils;
+
+/**
+ * Configurator to fetch or modify data from the Currencies.yml.
+ * 
+ * @author Wauzmons
+ */
+public class CurrencyConfigurator extends GlobalConfigurationUtils {
+	
+// Categories
+
+	/**
+	 * @return The keys of all categories.
+	 */
+	public static List<String> getAllCurrencyCategoryKeys() {
+		return new ArrayList<>(mainConfigGetKeys("Currency", null));
+	}
+	
+	/**
+	 * @param category The key of the category.
+	 * 
+	 * @return The display name of the category.
+	 */
+	public static String getCurrencyCategoryName(String category) {
+		return mainConfigGetString("Currency", category + ".name");
+	}
+	
+// Factions
+	
+	/**
+	 * @param category The key of the category.
+	 * 
+	 * @return The keys of the factions in the category.
+	 */
+	public static List<String> getCurrencyFactionKeys(String category) {
+		return new ArrayList<>(mainConfigGetKeys("Currency", category + ".factions"));
+	}
+	
+	/**
+	 * @param category The key of the category.
+	 * @param faction The key of the faction in the category.
+	 * 
+	 * @return The display name of the faction's curreny.
+	 */
+	public static String getCurrencyFactionDisplayName(String category, String faction) {
+		return mainConfigGetString("Currency", category + ".factions." + faction + ".name");
+	}
+	
+	/**
+	 * @param category The key of the category.
+	 * @param faction The key of the faction in the category.
+	 * 
+	 * @return The player config key of the faction's curreny.
+	 */
+	public static String getCurrencyFactionConfigName(String category, String faction) {
+		return mainConfigGetString("Currency", category + ".factions." + faction + ".key");
+	}
+
+}
---
+++ src/main/java/eu/wauz/wauzcore/system/WauzCurrency.java
@@ -0,0 +1,10 @@
+package eu.wauz.wauzcore.system;
+
+/**
+ * A currency or reputation type, generated from a currency config file.
+ * 
+ * @author Wauzmons
+ */
+public class WauzCurrency {
+
+}
