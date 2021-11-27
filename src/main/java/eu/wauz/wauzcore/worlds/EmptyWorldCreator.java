806ed0f0761b8cc51803212d5ab1f82553428871
Wauzmons <dev@wauz.eu>
2021-11-27 14:30:46 +0100

    Update EmptyWorldCreator.java

--- src/main/java/eu/wauz/wauzcore/worlds/EmptyWorldCreator.java
+++ src/main/java/eu/wauz/wauzcore/worlds/EmptyWorldCreator.java
@@ -1,11 +1,14 @@
 package eu.wauz.wauzcore.worlds;
 
-import java.util.Random;
+import java.util.Collections;
+import java.util.List;
 
-import org.bukkit.World;
 import org.bukkit.WorldCreator;
 import org.bukkit.block.Biome;
+import org.bukkit.generator.BiomeProvider;
 import org.bukkit.generator.ChunkGenerator;
+import org.bukkit.generator.WorldInfo;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * Represents various types of options that may be used to create an empty world.
@@ -21,18 +24,54 @@ public class EmptyWorldCreator extends WorldCreator {
 	 */
 	public EmptyWorldCreator(String name) {
 		super(name);
+		biomeProvider(new BiomeProvider() {
+			
+			@Override
+			public @NotNull List<Biome> getBiomes(@NotNull WorldInfo worldInfo) {
+				return Collections.singletonList(Biome.FOREST);
+			}
+			
+			@Override
+			public @NotNull Biome getBiome(@NotNull WorldInfo worldInfo, int x, int y, int z) {
+				return Biome.FOREST;
+			}
+			
+		});
 		generator(new ChunkGenerator() {
 			
 			@Override
-			public ChunkData generateChunkData(World world, Random random, int chunkX, int chunkZ, BiomeGrid biome) {
-				for(int x = 0; x < 16; x++) {
-					for(int y = 0; y < 256; y++) {
-						for(int z = 0; z < 16; z++) {
-							biome.setBiome(x, y, z, Biome.FOREST);
-						}
-					}
-				}
-				return createChunkData(world);
+			public boolean shouldGenerateBedrock() {
+				return false;
+			}
+			
+			@Override
+			public boolean shouldGenerateCaves() {
+				return false;
+			}
+			
+			@Override
+			public boolean shouldGenerateDecorations() {
+				return false;
+			}
+			
+			@Override
+			public boolean shouldGenerateMobs() {
+				return false;
+			}
+			
+			@Override
+			public boolean shouldGenerateNoise() {
+				return false;
+			}
+			
+			@Override
+			public boolean shouldGenerateStructures() {
+				return false;
+			}
+			
+			@Override
+			public boolean shouldGenerateSurface() {
+				return false;
 			}
 			
 		});
